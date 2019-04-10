b274c12ff73072e521d61e661d36079992074bf8
RSchmit <RSchmit@student.neumont.edu>
2019-04-10 15:58:28 -0600

    Started the Armored Boss Script and cleaned up and changed my working scene.

---
+++ Assets/Scripts/Boss Scripts/ArmoredPot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : Pot
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public class Armored_Shooting : State
+    {
+        public Player target = null;
+
+        public override void Enter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
