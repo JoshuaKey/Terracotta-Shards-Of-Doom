e3505a06d21b87fd544e9b58b7876e3f5c7f7f8d
JoshuaKey <keyjoshj98@yahoo.com>
2019-04-16 16:32:49 -0600

    Trying to add Resistances. Will is yelling at me

--- Assets/Scripts/Utility/DamageType.cs
+++ Assets/Scripts/Utility/DamageType.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Flags]
 public enum DamageType {
-    BASIC, // Pierce ?
-    FIRE,
-    ICE,
-    LIGHTNING,
-    EXPLOSIVE,
-    TRUE,
+    NONE = (1 << 0),
+    BASIC = (1 << 1), // Pierce ?
+    FIRE = (1 << 2),
+    ICE = (1 << 3),
+    LIGHTNING = (1 << 4),
+    EXPLOSIVE = (1 << 5),
+    TRUE = (1 << 6),
 }
