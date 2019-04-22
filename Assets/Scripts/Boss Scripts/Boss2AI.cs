ca2d442149fd2af3d25bbcbdadaaa09cf5b6e67d
RSchmit <RSchmit@student.neumont.edu>
2019-04-22 14:45:39 -0600

    The start of the second boss AI

---
+++ Assets/Scripts/Boss Scripts/Boss2AI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss2AI : Pot
+{
+    NavMeshAgent navMeshAgent = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        stateMachine = new StateMachine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Update();
+    }
+
+    private class Boss2AI_Panicked : TimedState
+    {
+        public Boss2AI_Panicked(float seconds) : base(seconds)
+        {
+
+        }
+
+        public override void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
