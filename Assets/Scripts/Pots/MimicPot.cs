5761cc0288e3207337c786f4f70ee227a8d1e601
Will <Eller.m.william@gmail.com>
2019-04-29 11:01:51 -0600

    Did the Lightmap for 2-2, Mimic Pot

---
+++ Assets/Scripts/Pots/MimicPot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimicPot : MonoBehaviour
+{
+
+    [SerializeField] public float aggroRadius = 5f; // this is when the pot first wakes up
+    [SerializeField] public float chaseRadius = 10.0f; // this is after the pot is awake
+    //This is all atack stuff and i currently dont wanna change it
+    [SerializeField] public float attackRadius = 2.5f;
+    [SerializeField] public float attackDuration = 0.25f;
+    [SerializeField] public float attackAngle = 70f;
+
+    [HideInInspector] public bool isAttacking = false;
+    [HideInInspector] public bool hasHitPlayer = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
