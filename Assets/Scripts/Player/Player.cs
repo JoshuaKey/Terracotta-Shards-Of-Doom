4d8a8bd7d3478553762b73d19b076dde9ee80255
ZSnowbank <ZSnowbank@gmail.com>
2019-04-30 12:39:38 -0600

    Changed Pause Menu button
    Cancel -> UI_Cancel

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -109,7 +109,7 @@ public class Player : MonoBehaviour {
         if (CanInteract) {
             UpdateInteractable();
         }
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetButtonDown("UI_Cancel"))
         {
             if (PauseMenu.Instance.activeSelf)
             {
