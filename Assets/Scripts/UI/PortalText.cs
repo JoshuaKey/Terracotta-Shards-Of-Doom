505d49270d53203aff4fc7a22c2d5ece66477e97
Melanie <timdog563@gmail.com>
2019-05-20 15:53:55 -0600

    Added Portal names in the hub and Sword Hitbox is better

---
+++ Assets/Scripts/UI/PortalText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortalText : MonoBehaviour
+{
+	[SerializeField] Text portalName = null;
+	[SerializeField] Player player = null;
+	[SerializeField] Canvas canvas = null;
+	[SerializeField] string text = "";
+	Vector3 textTransform;
+
+	float damageLife = 0;
+	// Use this for initialization
+	void Start()
+	{
+		player = Player.Instance;	
+		portalName.text = text;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(portalName.text != text)
+		{
+			portalName.text = text;
+		}
+
+		canvas.transform.rotation = Quaternion.identity;
+		canvas.transform.LookAt(player.transform.position);
+	}
+}
