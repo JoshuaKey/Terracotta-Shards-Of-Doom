934361683ba4aae812f9e2e4920384390bdb880f
ZSnowbank <ZSnowbank@gmail.com>
2019-05-20 15:53:00 -0600

    Cutscene Script created

---
+++ Assets/Scripts/Interactable/Cutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    private void Start()
+    {
+        Interactable interactble = GetComponent<Interactable>();
+        if (interactble == null) throw new System.Exception("A Cutscene tried to find an Interactable but couldn't.");
+
+        interactble.Subscribe(OnInteract);
+    }
+
+    public void OnInteract()
+    {
+        StartCoroutine(PlayCutscene());
+    }
+
+    IEnumerator PlayCutscene()
+    {
+        throw new System.NotImplementedException();
+    }
+}
