e8b6a327ffeca5d63ec81de0e3a9b3c8ce607688
ZSnowbank <ZSnowbank@gmail.com>
2019-05-22 14:20:53 -0600

    Cutscene mostly done

--- Assets/Scripts/Interactable/Cutscene.cs
+++ Assets/Scripts/Interactable/Cutscene.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Cutscene : MonoBehaviour
 {
+    [SerializeField] Camera camera;
+
+    private Camera mainCamera;
+
     private void Start()
     {
         Interactable interactble = GetComponent<Interactable>();
         if (interactble == null) throw new System.Exception("A Cutscene tried to find an Interactable but couldn't.");
 
         interactble.Subscribe(OnInteract);
+
+        mainCamera = Player.Instance.GetComponentInChildren<Camera>();
     }
 
     public void OnInteract()
@@ -19,6 +26,26 @@ public class Cutscene : MonoBehaviour
 
     IEnumerator PlayCutscene()
     {
-        throw new System.NotImplementedException();
+        Debug.Log($"Main Camera: {mainCamera}\nCutscene Camera: {camera}");
+
+        mainCamera.gameObject.SetActive(false);
+        camera.gameObject.SetActive(true);
+
+        NavMeshAgent[] navMeshAgents = EnemyManager.Instance.GetComponentsInChildren<NavMeshAgent>();
+
+        foreach(NavMeshAgent agent in navMeshAgents)
+        {
+            agent.enabled = false;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        mainCamera.gameObject.SetActive(true);
+        camera.gameObject.SetActive(false);
+
+        foreach(NavMeshAgent agent in navMeshAgents)
+        {
+            agent.enabled = true;
+        }
     }
 }
