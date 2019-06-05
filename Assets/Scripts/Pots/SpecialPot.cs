fee68914beb48eed08c86540bbfa5dcff255ab11
JoshuaKey <keyjoshj98@yahoo.com>
2019-06-05 15:34:46 -0600

    Added Controls to Puase Menu

--- Assets/Scripts/Pots/SpecialPot.cs
+++ Assets/Scripts/Pots/SpecialPot.cs
@@ -15,25 +15,28 @@ public class SpecialPot : MonoBehaviour {
     void Start() {
         if(enemy == null) { enemy = GetComponentInChildren<Enemy>(); }
 
-        if(PreviousPot != null) {
-            this.gameObject.SetActive(false);
-
-            PreviousPot.enemy.health.OnDeath += Spawn;
-        } else {
-            this.gameObject.SetActive(true);
-        }
-
         string levelName = LevelManager.Instance.GetLevelName();
+
         // Initialize value if it doesn't exist...
         if (!LevelManager.Instance.Levels[levelName].SpecialPots.ContainsKey(name)) {
             LevelManager.Instance.Levels[levelName].SpecialPots[name] = false;
         }
+
         // If already collected, modify material
         if (LevelManager.Instance.Levels[levelName].SpecialPots[name]) {
             renderer.material = HasCollectedMaterial;
             if (enemy.brokenPot != null) {
                 enemy.brokenPot.SetMaterial(HasCollectedMaterial);
             }
+        }
+
+        // Check if previous pot is Valid and we have not collected previous pot
+        if (PreviousPot != null && !LevelManager.Instance.Levels[levelName].SpecialPots[PreviousPot.name]) {
+            this.gameObject.SetActive(false);
+
+            PreviousPot.enemy.health.OnDeath += Spawn;
+        } else {
+            this.gameObject.SetActive(true);
         }
 
         this.enemy.health.OnDeath += OnDeath;
