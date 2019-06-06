5b8e37db65e9606575061c5db345c3e98194df66
ZSnowbank <ZSnowbank@gmail.com>
2019-06-06 15:39:32 -0600

    Credits finished

--- Assets/TextScroll.cs
+++ Assets/TextScroll.cs
@@ -1,25 +1,44 @@
-﻿using System.Collections;
+﻿using Luminosity.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TextScroll : MonoBehaviour
 {
-    [SerializeField] float speed = 1;
+    [SerializeField] float normalSpeed = 1;
+    [SerializeField] float buttonSpeed = 2;
+    [SerializeField] float endPos;
+    [SerializeField] UnityEvent onEnd;
 
+    Vector3 origin;
     RectTransform rect;
 
     private void Start()
     {
         rect = GetComponent<RectTransform>();
+        origin = rect.position;
 
         Debug.Log(Time.timeScale);
     }
 
     private void Update()
     {
+        float speed = InputManager.GetButton("UI_Submit") ? buttonSpeed : normalSpeed;
+
         Vector3 newPos = rect.position;
         newPos += (Vector3.up * speed * Time.deltaTime);
 
         rect.position = newPos;
+
+        if(rect.position.y >= endPos)
+        {
+            onEnd.Invoke();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(rect != null) rect.position = origin;
     }
 }
