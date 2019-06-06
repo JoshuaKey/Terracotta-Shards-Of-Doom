8b36fea2560ddb06d6e23f0915b0c79cb8d48825
ZSnowbank <ZSnowbank@gmail.com>
2019-06-06 14:08:02 -0600

    Created Text Scroll script

---
+++ Assets/TextScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScroll : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    RectTransform rect;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        Vector3 newPos = rect.position;
+        newPos += (Vector3.up * speed * Time.deltaTime);
+
+        rect.position = newPos;
+    }
+}
